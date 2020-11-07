0bc63ebea0ebc5bed8ee0549fd085a84f21c196e
Renault Scabora <renaultscabora@hotmail.com>
2020-11-07 11:04:34 -0300

    first commit

---
+++ ProLang.js
@@ -0,0 +1,17 @@
+(()=>{
+
+    "use strict";
+
+    let ProLang = new Object();
+
+    ProLang.parser = (string) => {
+
+        console.log(string);
+
+    }
+
+    window.ProLang = {
+        
+    }
+
+})(window);
